ca07a74eca8d2c1e881778b0bcb253ded1d8aba4
alejandro.nunez <alejandro.nunez@wundermanthompson.com>
2020-02-27 14:17:33 -0300

    compromiso added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 var conocimos = moment('2017-10-17'); //genero las fechas
 var novios = moment('2018-04-02');
+var comprometidos = moment('2020-02-24');
 var now = moment();
 
 var timeconocimos = moment.duration(now.diff(conocimos)); //resto las fechas
 var timenovios = moment.duration(now.diff(novios));
+var timecomprometidos = moment.duration(now.diff(comprometidos));
 var diffnovios = moment.preciseDiff(novios, now, true);
 var diffconocimos = moment.preciseDiff(conocimos, now, true); 
+var diffcomprometidos = moment.preciseDiff(comprometidos, now, true);
 
 var yearspace = document.getElementById("years"); //manipulo dom y muestro
 var monthspace = document.getElementById("months");
@@ -20,5 +23,12 @@ var daynovspace = document.getElementById("daysnov");
 yearnovspace.innerHTML = diffnovios.years;
 monthnovspace.innerHTML = diffnovios.months;
 daynovspace.innerHTML = diffnovios.days;
+
+var yearcompspace = document.getElementById("yearscom");
+var monthcompspace = document.getElementById("monthscom");
+var daycompspace = document.getElementById("dayscom");
+yearcompspace.innerHTML = diffcomprometidos.years;
+monthcompspace.innerHTML = diffcomprometidos.months;
+daycompspace.innerHTML = diffcomprometidos.days;
 
 

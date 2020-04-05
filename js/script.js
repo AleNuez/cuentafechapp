f723bfcbd824c72f762b6a149e222b73ffa698dd
alejandro.nunez <alejandro.nunez@wundermanthompson.com>
2020-04-05 17:10:20 -0300

    messing-things-around

--- js/script.js
+++ js/script.js
@@ -42,4 +42,11 @@ yearmarryspace.innerHTML = diffmarry.years;
 monthmarryspace.innerHTML = diffmarry.months;
 daymarryspace.innerHTML = diffmarry.days;
 
+function hideNav() {
+    var nav = document.querySelector("#navbar");
+    if (nav.style.display == "none") {
+        nav.style.display = "block";
+    } else nav.style.display = "none";
+}
+
 

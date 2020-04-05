d30cc379fb08330da8d9d43f762d47eaa6733202
alejandro.nunez <alejandro.nunez@wundermanthompson.com>
2020-04-05 19:07:03 -0300

    js-added

--- js/script.js
+++ js/script.js
@@ -14,30 +14,30 @@ var diffconocimos = moment.preciseDiff(conocimos, now, true);
 var diffcomprometidos = moment.preciseDiff(comprometidos, now, true);
 var diffmarry = moment.preciseDiff (now, casorio ,true);
 
-var yearspace = document.getElementById("years"); //manipulo dom y muestro
-var monthspace = document.getElementById("months");
-var dayspace = document.getElementById("days");
+var yearspace = document.getElementById("meet-years"); //manipulo dom y muestro
+var monthspace = document.getElementById("meet-months");
+var dayspace = document.getElementById("meet-days");
 yearspace.innerHTML = diffconocimos.years;
 monthspace.innerHTML = diffconocimos.months;
 dayspace.innerHTML = diffconocimos.days;
 
-var yearnovspace = document.getElementById("yearsnov");
-var monthnovspace = document.getElementById("monthsnov");
-var daynovspace = document.getElementById("daysnov");
+var yearnovspace = document.getElementById("novios-years");
+var monthnovspace = document.getElementById("novios-months");
+var daynovspace = document.getElementById("novios-days");
 yearnovspace.innerHTML = diffnovios.years;
 monthnovspace.innerHTML = diffnovios.months;
 daynovspace.innerHTML = diffnovios.days;
 
-var yearcompspace = document.getElementById("yearscom");
-var monthcompspace = document.getElementById("monthscom");
-var daycompspace = document.getElementById("dayscom");
+var yearcompspace = document.getElementById("compromiso-years");
+var monthcompspace = document.getElementById("compromiso-months");
+var daycompspace = document.getElementById("compromiso-days");
 yearcompspace.innerHTML = diffcomprometidos.years;
 monthcompspace.innerHTML = diffcomprometidos.months;
 daycompspace.innerHTML = diffcomprometidos.days;
 
-var yearmarryspace = document.getElementById("yearmarry");
-var monthmarryspace = document.getElementById("monthmarry");
-var daymarryspace = document.getElementById("daymarry");
+var yearmarryspace = document.getElementById("marriage-years");
+var monthmarryspace = document.getElementById("marriage-months");
+var daymarryspace = document.getElementById("marriage-days");
 yearmarryspace.innerHTML = diffmarry.years;
 monthmarryspace.innerHTML = diffmarry.months;
 daymarryspace.innerHTML = diffmarry.days;
